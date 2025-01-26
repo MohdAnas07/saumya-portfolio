eb5cf916e10aba256322c0a623d9b75b1898ada3
Mohd Anas <anas98144@gmail.com>
2025-01-27 01:42:09 +0530

    FIX IMAGES

--- components/HeroSection/components/Features.tsx
+++ components/HeroSection/components/Features.tsx
@@ -4,17 +4,17 @@ const Features = () => {
   return (
     <div className='flex items-center gap-3 space-x-4' >
       <div className="flex items-center space-x-4">
-        <img src="/images/skills_icons/figma-logo.png" alt="Figma" className="ml-3 w-8 h-8" />
+        <img src="/images/skills_icons/figma_logo.png" alt="Figma" className="ml-3 w-8 h-8" />
         <span className="font-nanum text-[37.5px] font-bold">Figma</span>
       </div>
       <img src="/images/decoresImg/shining_star.png" alt="Star" className="w-6 h-6" />
       <div className="flex items-center space-x-4">
-        <img src="/images/skills_icons/xd-logo.png" alt="Adobe XD" className="w-8 h-8" />
+        <img src="/images/skills_icons/xd_logo.png" alt="Adobe XD" className="w-8 h-8" />
         <span className="font-nanum text-[37.5px] font-bold">Adobe XD</span>
       </div>
       <img src="/images/decoresImg/shining_star.png" alt="Star" className="w-6 h-6" />
       <div className="flex items-center space-x-4">
-        <img src="/images/skills_icons/adobe-logo.png" alt="Illustrator" className="w-8 h-8" />
+        <img src="/images/skills_icons/adove_logo.png" alt="Illustrator" className="w-8 h-8" />
         <span className="font-nanum text-[37.5px] font-bold">Adobe Illustrator</span>
       </div>
     </div>
